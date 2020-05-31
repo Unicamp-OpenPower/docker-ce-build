84da71cb946d5c7d6e17b0f0945720d75e33ea46
Vinicius Espindola <vini.couto.e@gmail.com>
2020-05-31 12:59:44 +0000

    Added verbose to patching script

--- patch.py
+++ patch.py
@@ -1,5 +1,7 @@
 import re
 
+print("Running Patching Script...")
+
 deb_path = "docker-ce/components/packaging/deb/common/control"
 deb_ver = "containerd (>= 1.2.1)"
 
@@ -7,14 +9,19 @@ rpm_path = "docker-ce/components/packaging/rpm/SPECS/docker-ce.spec"
 rpm_ver = "Requires: containerd >= 1.2.1"
 
 # Update debian containerd dependency
+print("Patching DEB...")
 deb = open(deb_path, 'r')
 data = deb.read()
-data = re.sub(r'containerd.io \([^)]*\)', deb_ver, data)
-open(deb_path, 'w').write(data)
+new = re.sub(r'containerd.io \([^)]*\)', deb_ver, data)
+assert data != new, "Nothing was changed in the file."
+open(deb_path, 'w').write(new)
 
 # Update rpm containerd dependency
+print("Patching RPM...")
 rpm = open(rpm_path, 'r')
 data = rpm.read()
-data = re.sub(r'Requires: containerd.io [^\n]*', rpm_ver, data)
-open(rpm_path, 'w').write(data)
-print(data)
+new = re.sub(r'Requires: containerd.io [^\n]*', rpm_ver, data)
+assert data != new, "Nothing was changed in the file."
+open(rpm_path, 'w').write(new)
+
+print("DONE Patching")
